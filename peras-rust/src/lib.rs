0d12f016fb73b5ca44586c3ecb81d7d09d40f825
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-02-21 10:22:19 +0100

    Start implementing Rust node
    Just make sure we can call the rust side from the Haskell side
    assuming libraries are correctly installed (How?)

--- peras-rust/src/lib.rs
+++ peras-rust/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct PerasNode {
+    node_id: String,
+    stake: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Create a new Peras node
+///
+#[no_mangle]
+pub unsafe extern "C" fn start_node(_node_id: *const char, _node_stake: f64) -> *mut PerasNode {
+    panic!("not implemented")
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub unsafe extern "C" fn stop_node(_node: *mut PerasNode) {
+    panic!("not implemented")
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn send_message(_node: *mut PerasNode, _buf: *const u8, _len: usize) {
+    panic!("not implemented")
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn receive_message(
+    _node: *mut PerasNode,
+    _buf: *mut u8,
+    _len: usize,
+) -> usize {
+    panic!("not implemented")
 }
