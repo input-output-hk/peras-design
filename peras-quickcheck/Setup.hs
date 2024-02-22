fef6140b8a4bc13f61d80de32421418dd2cee0e4
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-02-22 09:10:32 +0100

    Add custom setup to automatically build and link peras-rust

---
+++ peras-quickcheck/Setup.hs
@@ -0,0 +1,81 @@
+import Prelude
+
+import Data.Maybe (
+  fromJust,
+ )
+import Distribution.Simple (
+  Args,
+  UserHooks (..),
+  defaultMainWithHooks,
+  simpleUserHooks,
+ )
+import Distribution.Simple.LocalBuildInfo (
+  LocalBuildInfo (..),
+ )
+import Distribution.Simple.Setup (
+  BuildFlags (..),
+  ConfigFlags (..),
+  fromFlag,
+ )
+import Distribution.Simple.UserHooks (
+  UserHooks (..),
+ )
+import Distribution.Simple.Utils (
+  rawSystemProc,
+ )
+import System.Directory (
+  getCurrentDirectory,
+ )
+import System.FilePath ((</>))
+import System.Process (cwd, proc)
+
+import qualified Distribution.PackageDescription as Pkg
+
+main :: IO ()
+main =
+  defaultMainWithHooks
+    simpleUserHooks
+      { confHook = rustConfHook
+      , buildHook = rustBuildHook
+      }
+
+rustConfHook ::
+  (Pkg.GenericPackageDescription, Pkg.HookedBuildInfo) ->
+  ConfigFlags ->
+  IO LocalBuildInfo
+rustConfHook (description, buildInfo) flags = do
+  localBuildInfo <- confHook simpleUserHooks (description, buildInfo) flags
+  let packageDescription = localPkgDescr localBuildInfo
+  let library = fromJust $ Pkg.library packageDescription
+  let libraryBuildInfo = Pkg.libBuildInfo library
+  dir <- getCurrentDirectory
+  return
+    localBuildInfo
+      { localPkgDescr =
+          packageDescription
+            { Pkg.library =
+                Just
+                  library
+                    { Pkg.libBuildInfo =
+                        libraryBuildInfo
+                          { Pkg.extraLibDirs = (dir </> "../peras-rust/target/debug") : Pkg.extraLibDirs libraryBuildInfo
+                          , Pkg.includes = "peras.h" : Pkg.includes libraryBuildInfo
+                          , Pkg.includeDirs = (dir </> "../peras-rust/") : Pkg.includeDirs libraryBuildInfo
+                          }
+                    }
+            }
+      }
+
+rustBuildHook ::
+  Pkg.PackageDescription ->
+  LocalBuildInfo ->
+  UserHooks ->
+  BuildFlags ->
+  IO ()
+rustBuildHook description localBuildInfo hooks flags = do
+  putStrLn "[rust] Compiling Rust dependencies..."
+  putStrLn "[rust] cargo build --release"
+  dir <- getCurrentDirectory -- Assume it's the directory containing the .cabal file
+  let cargo = proc "cargo" ["build"]
+  rawSystemProc (fromFlag $ buildVerbosity flags) cargo{cwd = Just (dir </> ".." </> "peras-rust")}
+  buildHook simpleUserHooks description localBuildInfo hooks flags
