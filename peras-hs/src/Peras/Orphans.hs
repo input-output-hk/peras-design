ad68cf725e511443d4c71777594f42a7c7b92a48
Brian W Bush <brian.bush@iohk.io>
2024-02-27 08:29:28 -0700

    Switched to using generically-derived JSON.

--- peras-hs/src/Peras/Orphans.hs
+++ peras-hs/src/Peras/Orphans.hs
@@ -1,33 +1,35 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Peras.Orphans where
 
-import Data.Bifunctor (first)
+import Data.Aeson (
+  FromJSON (parseJSON),
+  FromJSONKey,
+  ToJSON (toJSON),
+  ToJSONKey,
+  Value (String),
+  withText,
+ )
+import Data.Aeson.Types (parseFail)
 import Data.String (IsString (..))
 import GHC.Generics (Generic)
-import Peras.Block (Block (..))
+import Peras.Block (Block (..), Party (..))
 import Peras.Chain (Chain (..), RoundNumber (..), Vote (..))
 import Peras.Crypto (Hash (..), LeadershipProof (..), MembershipProof (..), Signature (..), VerificationKey (..))
 import Peras.Message (Message (..), NodeId (..))
+import Text.Read (Read (readPrec))
 
-import qualified Data.Aeson as A
-import qualified Data.Aeson.Types as A
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.Text as T
-import Text.Read (Read (readPrec))
 
 -- Only used for deriving instances of similar types.
+
 newtype Bytes = Bytes {getBytes :: BS.ByteString}
 
 instance Read Bytes where
@@ -41,211 +43,119 @@ instance Show Bytes where
 instance IsString Bytes where
   fromString = either error Bytes . B16.decode . BS8.pack
 
-instance A.FromJSON Bytes where
-  parseJSON = A.withText "Base 16 Bytes" $ either A.parseFail (pure . Bytes) . B16.decode . BS8.pack . T.unpack
+instance FromJSON Bytes where
+  parseJSON = withText "Base 16 Bytes" $ either parseFail (pure . Bytes) . B16.decode . BS8.pack . T.unpack
 
-instance A.ToJSON Bytes where
-  toJSON = A.String . T.pack . init . tail . show
+instance ToJSON Bytes where
+  toJSON = String . T.pack . init . tail . show
+
+deriving via Bytes instance FromJSON BS.ByteString
+deriving via Bytes instance ToJSON BS.ByteString
+
+-- Orphans for `Peras.Block`.
+
+deriving stock instance Generic Party
+deriving stock instance Ord Party
+deriving stock instance Read Party
+deriving stock instance Show Party
+instance FromJSON Party
+instance ToJSON Party
+instance FromJSONKey Party
+instance ToJSONKey Party
 
 deriving stock instance Generic Block
 deriving stock instance Ord Block
 deriving stock instance Read Block
 deriving stock instance Show Block
+instance FromJSON Block
+instance ToJSON Block
+instance FromJSONKey Block
+instance ToJSONKey Block
 
-instance A.FromJSON Block where
-  parseJSON =
-    A.withObject "Block" $
-      \o ->
-        do
-          slotNumber <- o A..: "slotNo"
-          creatorId <- o A..: "creatorId"
-          parentBlock <- o A..: "parentBlock"
-          includedVotes <- o A..: "includedVotes"
-          leadershipProof <- o A..: "leadershipProof"
-          payload <- o A..: "payload"
-          signature <- o A..: "signature"
-          pure Block{..}
-
-instance A.ToJSON Block where
-  toJSON Block{..} =
-    A.object
-      [ "slotNo" A..= slotNumber
-      , "creatorId" A..= creatorId
-      , "parentBlock" A..= parentBlock
-      , "includedVotes" A..= includedVotes
-      , "leadershipProof" A..= leadershipProof
-      , "payload" A..= payload
-      , "signature" A..= signature
-      ]
+-- Orphans for `Peras.Chain`.
 
 deriving stock instance Generic RoundNumber
 deriving stock instance Ord RoundNumber
 deriving stock instance Read RoundNumber
 deriving stock instance Show RoundNumber
-
-instance A.ToJSONKey (Maybe Block) where
-  toJSONKey =
-    A.toJSONKeyText $
-      \case
-        Nothing -> ""
-        Just Block{signature = s} -> T.pack . BS8.unpack . B16.encode $ Peras.Crypto.signature s
-
-instance A.FromJSON RoundNumber where
-  parseJSON =
-    A.withObject "RoundNumber" $
-      \o ->
-        do
-          roundNumber <- o A..: "roundNumber"
-          pure RoundNumber{..}
-
-instance A.ToJSON RoundNumber where
-  toJSON RoundNumber{..} =
-    A.object ["roundNumber" A..= roundNumber]
+instance FromJSON RoundNumber
+instance ToJSON RoundNumber
 
 deriving stock instance Generic v => Generic (Vote v)
 deriving stock instance Ord v => Ord (Vote v)
 deriving stock instance Read v => Read (Vote v)
 deriving stock instance Show v => Show (Vote v)
-
-instance A.FromJSON v => A.FromJSON (Vote v) where
-  parseJSON =
-    A.withObject "Block" $
-      \o ->
-        do
-          votingRound <- o A..: "votingRound"
-          creatorId <- o A..: "creatorId"
-          committeeMembershipProof <- o A..: "committeeMembershipProof"
-          blockHash <- o A..: "blockHash"
-          signature <- o A..: "signature"
-          pure MkVote{..}
-
-instance A.ToJSON v => A.ToJSON (Vote v) where
-  toJSON MkVote{..} =
-    A.object
-      [ "votingRound" A..= votingRound
-      , "creatorId" A..= creatorId
-      , "committeeMembershipProof" A..= committeeMembershipProof
-      , "blockHash" A..= blockHash
-      , "signature" A..= signature
-      ]
+instance (Generic v, FromJSON v) => FromJSON (Vote v)
+instance (Generic v, ToJSON v) => ToJSON (Vote v)
 
 deriving stock instance Generic Chain
 deriving stock instance Ord Chain
 deriving stock instance Read Chain
 deriving stock instance Show Chain
+instance FromJSON Chain
+instance ToJSON Chain
 
-instance A.FromJSON Chain where
-  parseJSON =
-    A.withObject "Chain" $
-      \o ->
-        do
-          blocks <- o A..: "blocks"
-          votes <- o A..: "votes"
-          pure MkChain{..}
-
-instance A.ToJSON Chain where
-  toJSON MkChain{..} =
-    A.object
-      [ "blocks" A..= blocks
-      , "votes" A..= votes
-      ]
+-- Orphans for `Peras.Crypto`.
 
 deriving stock instance Generic Hash
 deriving stock instance Ord Hash
 deriving via Bytes instance Read Hash
 deriving via Bytes instance Show Hash
 deriving via Bytes instance IsString Hash
-deriving via Bytes instance A.FromJSON Hash
-deriving via Bytes instance A.ToJSON Hash
-
-deriving stock instance Eq Message
-deriving stock instance Generic Message
-deriving stock instance Ord Message
-deriving stock instance Read Message
-deriving stock instance Show Message
-
-instance A.FromJSON Message where
-  parseJSON =
-    A.withObject "Message" $
-      \o ->
-        do
-          input <- o A..: "input"
-          case input of
-            "NextSlot" -> NextSlot <$> o A..: "slot"
-            "SomeBlock" -> SomeBlock <$> o A..: "block"
-            "NewChain" -> NewChain <$> o A..: "chain"
-            _ -> A.parseFail $ "Illegal input: " <> input
-
-instance A.ToJSON Message where
-  toJSON (NextSlot slot) =
-    A.object
-      [ "input" A..= ("NextSlot" :: String)
-      , "slot" A..= slot
-      ]
-  toJSON (SomeBlock block) =
-    A.object
-      [ "input" A..= ("SomeBlock" :: String)
-      , "block" A..= block
-      ]
-  toJSON (NewChain chain) =
-    A.object
-      [ "input" A..= ("NewChain" :: String)
-      , "chain" A..= chain
-      ]
-
-deriving stock instance Generic LeadershipProof
-deriving stock instance Ord LeadershipProof
-deriving via Bytes instance Read LeadershipProof
-deriving via Bytes instance Show LeadershipProof
-deriving via Bytes instance IsString LeadershipProof
-deriving via Bytes instance A.FromJSON LeadershipProof
-deriving via Bytes instance A.ToJSON LeadershipProof
+instance FromJSON Hash
+instance ToJSON Hash
 
 deriving stock instance Generic MembershipProof
 deriving stock instance Ord MembershipProof
 deriving via Bytes instance Read MembershipProof
 deriving via Bytes instance Show MembershipProof
 deriving via Bytes instance IsString MembershipProof
-deriving via Bytes instance A.FromJSON MembershipProof
-deriving via Bytes instance A.ToJSON MembershipProof
+instance FromJSON MembershipProof
+instance ToJSON MembershipProof
 
-deriving stock instance Eq NodeId
-deriving stock instance Generic NodeId
-deriving stock instance Ord NodeId
-
-instance Read NodeId where
-  readsPrec _ = pure . (,"") . MkNodeId
-
-instance Show NodeId where
-  show = nodeId
-
-instance IsString NodeId where
-  fromString = MkNodeId
-
-instance A.FromJSON NodeId where
-  parseJSON = A.withText "NodeId" $ pure . MkNodeId . T.unpack
-
-instance A.ToJSON NodeId where
-  toJSON = A.String . T.pack . nodeId
-
-instance A.FromJSONKey NodeId where
-  fromJSONKey = A.FromJSONKeyTextParser $ pure . MkNodeId . T.unpack
-
-instance A.ToJSONKey NodeId where
-  toJSONKey = A.toJSONKeyText $ T.pack . nodeId
+deriving stock instance Generic LeadershipProof
+deriving stock instance Ord LeadershipProof
+deriving via Bytes instance Read LeadershipProof
+deriving via Bytes instance Show LeadershipProof
+deriving via Bytes instance IsString LeadershipProof
+instance FromJSON LeadershipProof
+instance ToJSON LeadershipProof
 
 deriving stock instance Generic Signature
 deriving stock instance Ord Signature
 deriving via Bytes instance Read Signature
 deriving via Bytes instance Show Signature
 deriving via Bytes instance IsString Signature
-deriving via Bytes instance A.FromJSON Signature
-deriving via Bytes instance A.ToJSON Signature
+instance FromJSON Signature
+instance ToJSON Signature
 
 deriving stock instance Generic VerificationKey
 deriving stock instance Ord VerificationKey
 deriving via Bytes instance Read VerificationKey
 deriving via Bytes instance Show VerificationKey
 deriving via Bytes instance IsString VerificationKey
-deriving via Bytes instance A.FromJSON VerificationKey
-deriving via Bytes instance A.ToJSON VerificationKey
+instance FromJSON VerificationKey
+instance ToJSON VerificationKey
+
+-- Orphans for `Peras.Message`.
+
+deriving stock instance Eq NodeId
+deriving stock instance Generic NodeId
+deriving stock instance Ord NodeId
+deriving stock instance Read NodeId
+deriving stock instance Show NodeId
+instance IsString NodeId where
+  fromString = MkNodeId
+
+instance FromJSON NodeId
+instance ToJSON NodeId
+instance FromJSONKey NodeId
+instance ToJSONKey NodeId
+
+deriving stock instance Eq Message
+deriving stock instance Generic Message
+deriving stock instance Ord Message
+deriving stock instance Read Message
+deriving stock instance Show Message
+instance FromJSON Message
+instance ToJSON Message
