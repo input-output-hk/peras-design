b9f0af35c62b038a38853e6fb9c32e09f71a76c8
Brian W Bush <brian.bush@iohk.io>
2024-03-22 10:13:08 -0600

    Upgraded `Peras.Hash.Hash` types.

--- peras-hs/src/Peras/Orphans.hs
+++ peras-hs/src/Peras/Orphans.hs
@@ -20,7 +20,6 @@ import Data.Aeson (
  )
 import Data.Aeson.Types (parseFail, toJSONKeyText)
 import Data.Bifunctor (bimap)
-import Data.Hashable (Hashable (..))
 import Data.String (IsString (..))
 import GHC.Generics (Generic)
 import Peras.Block (Block (..), BlockBody (..), Certificate (..), Party (..))
@@ -33,6 +32,7 @@ import Text.Read (Read (readPrec))
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Char8 as BS8
+import qualified Data.Hashable as H (Hashable (..))
 import qualified Data.Text as T
 
 -- Only used for deriving instances of similar types.
@@ -51,7 +51,7 @@ instance Show Bytes where
 instance IsString Bytes where
   fromString = either error Bytes . B16.decode . BS8.pack
 
-deriving anyclass instance Hashable Bytes
+instance H.Hashable Bytes
 
 instance FromJSON Bytes where
   parseJSON = withText "Base 16 Bytes" $ either parseFail (pure . Bytes) . B16.decode . BS8.pack . T.unpack
@@ -77,7 +77,7 @@ deriving stock instance Generic Party
 deriving stock instance Ord Party
 deriving stock instance Read Party
 deriving stock instance Show Party
-deriving anyclass instance Hashable Party
+deriving anyclass instance H.Hashable Party
 instance FromJSON Party
 instance ToJSON Party
 instance FromJSONKey Party
@@ -87,7 +87,7 @@ deriving stock instance Generic Certificate
 deriving stock instance Ord Certificate
 deriving stock instance Read Certificate
 deriving stock instance Show Certificate
-instance Hashable Certificate
+instance H.Hashable Certificate
 instance FromJSON Certificate
 instance ToJSON Certificate
 
@@ -95,7 +95,7 @@ deriving stock instance Generic Block
 deriving stock instance Ord Block
 deriving stock instance Read Block
 deriving stock instance Show Block
-deriving anyclass instance Hashable Block
+deriving anyclass instance H.Hashable Block
 instance FromJSON Block
 instance ToJSON Block
 
@@ -103,7 +103,7 @@ deriving stock instance Generic BlockBody
 deriving stock instance Ord BlockBody
 deriving stock instance Read BlockBody
 deriving stock instance Show BlockBody
-deriving anyclass instance Hashable BlockBody
+deriving anyclass instance H.Hashable BlockBody
 instance FromJSON BlockBody
 instance ToJSON BlockBody
 
@@ -113,7 +113,7 @@ deriving stock instance Generic RoundNumber
 deriving stock instance Ord RoundNumber
 deriving stock instance Read RoundNumber
 deriving stock instance Show RoundNumber
-deriving anyclass instance Hashable RoundNumber
+deriving anyclass instance H.Hashable RoundNumber
 deriving newtype instance FromJSON RoundNumber
 deriving newtype instance ToJSON RoundNumber
 deriving newtype instance FromJSONKey RoundNumber
@@ -139,7 +139,7 @@ deriving stock instance Generic Vote
 deriving stock instance Ord Vote
 deriving stock instance Read Vote
 deriving stock instance Show Vote
-deriving anyclass instance Hashable Vote
+deriving anyclass instance H.Hashable Vote
 instance FromJSON Vote
 instance ToJSON Vote
 
@@ -162,13 +162,13 @@ instance FromJSON a => FromJSONKey (Hash a) where
 instance ToJSON a => ToJSONKey (Hash a) where
   toJSONKey = toJSONKeyText $ T.pack . init . tail . show . hashBytes
 
-instance Hashable (Hash a) where
-  hash = hash . hashBytes
-  hashWithSalt = (. hashBytes) . hashWithSalt
+instance H.Hashable (Hash a) where
+  hash = H.hash . hashBytes
+  hashWithSalt = (. hashBytes) . H.hashWithSalt
 
 deriving stock instance Generic MembershipProof
 deriving stock instance Ord MembershipProof
-deriving anyclass instance Hashable MembershipProof
+deriving anyclass instance H.Hashable MembershipProof
 deriving via Bytes instance Read MembershipProof
 deriving via Bytes instance Show MembershipProof
 deriving via Bytes instance IsString MembershipProof
@@ -177,7 +177,7 @@ deriving via Bytes instance ToJSON MembershipProof
 
 deriving stock instance Generic LeadershipProof
 deriving stock instance Ord LeadershipProof
-deriving anyclass instance Hashable LeadershipProof
+deriving anyclass instance H.Hashable LeadershipProof
 deriving via Bytes instance Read LeadershipProof
 deriving via Bytes instance Show LeadershipProof
 deriving via Bytes instance IsString LeadershipProof
@@ -186,7 +186,7 @@ deriving via Bytes instance ToJSON LeadershipProof
 
 deriving stock instance Generic Signature
 deriving stock instance Ord Signature
-deriving anyclass instance Hashable Signature
+deriving anyclass instance H.Hashable Signature
 deriving via Bytes instance Read Signature
 deriving via Bytes instance Show Signature
 deriving via Bytes instance IsString Signature
@@ -195,7 +195,7 @@ deriving via Bytes instance ToJSON Signature
 
 deriving stock instance Generic VerificationKey
 deriving stock instance Ord VerificationKey
-deriving anyclass instance Hashable VerificationKey
+deriving anyclass instance H.Hashable VerificationKey
 deriving via Bytes instance Read VerificationKey
 deriving via Bytes instance Show VerificationKey
 deriving via Bytes instance IsString VerificationKey
@@ -212,14 +212,14 @@ deriving newtype instance FromJSON NodeId
 deriving newtype instance ToJSON NodeId
 deriving newtype instance FromJSONKey NodeId
 deriving newtype instance ToJSONKey NodeId
-deriving newtype instance Hashable NodeId
+deriving newtype instance H.Hashable NodeId
 
 deriving stock instance Eq Message
 deriving stock instance Ord Message
 deriving stock instance Generic Message
 deriving instance Read Message
 deriving instance Show Message
-deriving anyclass instance Hashable Message
+deriving anyclass instance H.Hashable Message
 instance FromJSON Message
 instance ToJSON Message
 
@@ -227,7 +227,7 @@ deriving stock instance Eq UniqueId
 deriving stock instance Ord UniqueId
 deriving via Bytes instance Read UniqueId
 deriving via Bytes instance Show UniqueId
-deriving via Bytes instance Hashable UniqueId
+deriving via Bytes instance H.Hashable UniqueId
 deriving via Bytes instance IsString UniqueId
 deriving via Bytes instance FromJSON UniqueId
 deriving via Bytes instance ToJSON UniqueId
@@ -241,7 +241,7 @@ deriving stock instance Generic Event
 deriving stock instance Ord Event
 deriving stock instance Read Event
 deriving stock instance Show Event
-deriving anyclass instance Hashable Event
+deriving anyclass instance H.Hashable Event
 instance FromJSON Event
 instance ToJSON Event
 
@@ -250,6 +250,6 @@ deriving stock instance Generic Rollback
 deriving stock instance Ord Rollback
 deriving stock instance Read Rollback
 deriving stock instance Show Rollback
-deriving anyclass instance Hashable Rollback
+deriving anyclass instance H.Hashable Rollback
 instance FromJSON Rollback
 instance ToJSON Rollback
--- peras-iosim/src/Peras/IOSim/Hash.hs
+++ peras-iosim/src/Peras/IOSim/Hash.hs
@@ -11,11 +11,11 @@ module Peras.IOSim.Hash (
   hashVote,
 ) where
 
-import Peras.Block as Block (Block (bodyHash), BlockBody (blockHash))
+import Peras.Block as Block (Block (bodyHash), BlockBody (blockHash), Tx)
 import Peras.Chain as Chain (Vote (signature))
 import Peras.Crypto as Crypto (Hash (Hash), Signature (bytes))
 
-type BlockHash = Hash
+type BlockHash = Hash [Tx]
 
 hashBlock :: Block -> BlockHash
 hashBlock = Block.bodyHash
@@ -27,9 +27,9 @@ hashTip :: [Block] -> BlockHash
 hashTip [] = genesisHash
 hashTip (block : _) = hashBlock block
 
-type BodyHash = Hash
+type BodyHash = Hash [Tx]
 
-type VoteHash = Hash
+type VoteHash = Hash Vote
 
 hashVote :: Vote -> VoteHash
 hashVote = Crypto.Hash . Crypto.bytes . Chain.signature
