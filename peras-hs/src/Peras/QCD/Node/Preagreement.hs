56a57bb1475b60c76904909ead532cc0967a84de
Brian W Bush <brian.bush@iohk.io>
2024-05-09 08:51:53 -0600

    Implemented pre-agreement.

--- peras-hs/src/Peras/QCD/Node/Preagreement.hs
+++ peras-hs/src/Peras/QCD/Node/Preagreement.hs
@@ -1,9 +1,11 @@
 module Peras.QCD.Node.Preagreement where
 
-import Peras.QCD.Node.Model (NodeState, currentSlot, peras)
+import Numeric.Natural (Natural)
+import Peras.QCD.Node.Model (NodeState, currentSlot, peras, preferredChain)
 import Peras.QCD.Protocol (ParamSymbol (L))
 import Peras.QCD.State (use)
-import Peras.QCD.Types (Block)
+import Peras.QCD.Types (Block (slot), Slot, chainBlocks)
+import Peras.QCD.Util ((⇉))
 
 import Peras.QCD.Types.Instances ()
 
@@ -12,4 +14,11 @@ preagreement =
   do
     l <- peras L
     now <- use currentSlot
-    pure Nothing
+    ((use preferredChain ⇉ chainBlocks) ⇉ dropWhile (newerThan l now))
+      ⇉ foundBlock
+ where
+  newerThan :: Natural -> Slot -> Block -> Bool
+  newerThan l now block = slot block + l > now
+  foundBlock :: [Block] -> Maybe Block
+  foundBlock [] = Nothing
+  foundBlock (block : _) = Just block
