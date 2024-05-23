6be01e3370fd6f390d308343f675d91a43db92d3
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-05-23 09:35:26 +0200

    Extract modulus computation to own module

--- peras-quickcheck/src/Peras/Abstract/Protocol/Voting.hs
+++ peras-quickcheck/src/Peras/Abstract/Protocol/Voting.hs
@@ -7,12 +7,8 @@ module Peras.Abstract.Protocol.Voting where
 import Control.Concurrent.Class.MonadSTM (MonadSTM (..))
 import Control.Monad.Except (ExceptT (..), runExceptT)
 import Control.Monad.Trans (lift)
-import Data.Bits (FiniteBits, countLeadingZeros, countTrailingZeros, finiteBitSize, (.&.), (.<<.))
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
-import Data.Serialize (getWord64le, runGet)
 import qualified Data.Set as Set
-import Data.Word (Word64)
+import Peras.Abstract.Protocol.BytesModulo (modulo)
 import Peras.Abstract.Protocol.Crypto (createMembershipProof, createSignedVote)
 import Peras.Abstract.Protocol.Types (PerasState (..), Voting)
 import Peras.Block (Party (..))
@@ -45,38 +41,3 @@ voting params party perasState roundNumber preagreement diffuseVote = runExceptT
 isCommitteeMember :: Party -> RoundNumber -> Bool
 isCommitteeMember MkParty{pkey = MkVerificationKey bytes} roundNumber =
   bytes `modulo` fromIntegral roundNumber == 0
-
-modulo :: ByteString -> Word64 -> Word64
-modulo bytes n =
-  if isPowerOf2 n
-    then modPowerOf2 bytes n
-    else modNonPowerOf2 bytes n
-
-modNonPowerOf2 :: ByteString -> Word64 -> Word64
-modNonPowerOf2 bytes n =
-  if i >= d * n
-    then error $ "failed: i = " <> show i <> ", d = " <> show d <> ", n = " <> show n <> ", k = " <> show k
-    else i `mod` n
- where
-  k' = 1 .<<. k
-  k = logBase2 (n * εFail)
-  d = k' `div` n
-  i = modPowerOf2 bytes k'
-
-εFail :: Word64
-εFail = 1 .<<. 40 -- roughly 1 in 10 billions
-
-logBase2 :: FiniteBits b => b -> Int
-logBase2 x = finiteBitSize x - 1 - countLeadingZeros x
-
-modPowerOf2 :: ByteString -> Word64 -> Word64
-modPowerOf2 bytes n =
-  let r = fromBytesLE bytes
-   in (n - 1) .&. r
-
-fromBytesLE :: ByteString -> Word64
-fromBytesLE = either error id . runGet getWord64le . BS.take 8
-
-isPowerOf2 :: Word64 -> Bool
-isPowerOf2 n =
-  countLeadingZeros n + countTrailingZeros n == 63
