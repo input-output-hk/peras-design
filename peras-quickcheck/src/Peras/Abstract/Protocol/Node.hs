81a5a32e5bf3766e7b56c839fa53a95f3c2f3da1
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-05-24 10:25:55 +0200

    Flesh out a Network module to "run" a node
    Also concretise the type of tick to ensure the NodeState does not
    depend on the (MonadState m) which makes things annoying. In general,
    we should get rid of transformers stack as this makes the code much
    harder to grasp for non experienced Haskellers.

--- peras-quickcheck/src/Peras/Abstract/Protocol/Node.hs
+++ peras-quickcheck/src/Peras/Abstract/Protocol/Node.hs
@@ -7,7 +7,8 @@ import Control.Arrow ((&&&))
 import Control.Concurrent.Class.MonadSTM (MonadSTM (..))
 import Control.Monad (when)
 import Control.Monad.Except (ExceptT (ExceptT), runExceptT)
-import Control.Monad.State (MonadState, gets, lift, modify')
+import Control.Monad.State (StateT, gets, lift, modify')
+import Data.Set (Set)
 import Peras.Abstract.Protocol.BlockCreation (blockCreation)
 import Peras.Abstract.Protocol.Diffusion (Diffuser (..), defaultDiffuser, diffuseChain, diffuseVote)
 import Peras.Abstract.Protocol.Fetching (fetching)
@@ -15,6 +16,7 @@ import Peras.Abstract.Protocol.Preagreement (preagreement)
 import Peras.Abstract.Protocol.Types (Payload, PerasParams (perasU), PerasResult, PerasState, defaultParams, initialPerasState, systemStart)
 import Peras.Abstract.Protocol.Voting (voting)
 import Peras.Block (Party)
+import Peras.Chain (Chain, Vote)
 import Peras.Numbering (SlotNumber)
 
 data NodeState m = MkNodeState
@@ -33,29 +35,32 @@ initialNodeState self clock =
     diffuserVar <- newTVarIO defaultDiffuser
     pure MkNodeState{..}
 
-tick :: (MonadSTM m, MonadState (NodeState m) m) => Payload -> m (PerasResult ())
-tick payload =
+tick :: MonadSTM m => Payload -> StateT (NodeState m) m (PerasResult ())
+tick payload = do
+  params <- gets protocol
+  party <- gets self
+  state <- gets stateVar
+  diffuser <- gets diffuserVar
+  -- 1. Increment clock.
+  s <- gets $ (1 +) . clock
+  modify' $ \node -> node{clock = s}
+  let r = fromIntegral $ fromIntegral s `div` perasU params
+      newRound = fromIntegral s `mod` perasU params == 0
+  -- 2. Get votes and chains from the network.
+  (newChains, newVotes) <- lift (fetchNewChainsAndVotes diffuser)
   runExceptT $ do
-    params <- gets protocol
-    party <- gets self
-    state <- gets stateVar
-    diffuser <- gets diffuserVar
-    -- 1. Increment clock.
-    s <- gets $ (1 +) . clock
-    modify' $ \node -> node{clock = s}
-    let r = fromIntegral $ fromIntegral s `div` perasU params
-        newRound = fromIntegral s `mod` perasU params == 0
-    -- 2. Get votes and chains from the network.
-    (newChains, newVotes) <-
-      lift . atomically $ do
-        result <- (pendingChains &&& pendingVotes) <$> readTVar diffuser
-        modifyTVar diffuser $ \d -> d{pendingChains = mempty, pendingVotes = mempty}
-        pure result
     -- 3. Invoke fetching.
-    ExceptT $ fetching params party state s newChains newVotes
+    ExceptT $ lift $ fetching params party state s newChains newVotes
     -- 4. Invoke block creation if leader.
-    ExceptT $ blockCreation params party state s payload (diffuseChain diffuser)
+    ExceptT $ lift $ blockCreation params party state s payload (diffuseChain diffuser)
     -- 5. Invoke voting if committee member.
     when newRound $
       ExceptT $
-        voting params party state r preagreement (diffuseVote diffuser)
+        lift $
+          voting params party state r preagreement (diffuseVote diffuser)
+
+fetchNewChainsAndVotes :: MonadSTM m => TVar m Diffuser -> m (Set Chain, Set Vote)
+fetchNewChainsAndVotes diffuser = atomically $ do
+  result <- (pendingChains &&& pendingVotes) <$> readTVar diffuser
+  modifyTVar diffuser $ \d -> d{pendingChains = mempty, pendingVotes = mempty}
+  pure result
