b9e8e39b3232979011d3c8bff7d91ebbc3bd54df
Brian W Bush <brian.bush@iohk.io>
2024-06-02 14:37:09 -0600

    Separated visualization from simulation, and added read-me file.

--- peras-simulation/app/Simulate.hs
+++ peras-simulation/app/Simulate.hs
@@ -9,18 +9,17 @@ import Paths_peras_simulation (version)
 import Peras.Abstract.Protocol.Environment (mkSimpleScenario)
 import Peras.Abstract.Protocol.Network (simulate, simulateNetwork)
 import Peras.Abstract.Protocol.Trace (perasTracer)
-import Peras.Abstract.Protocol.Visualizer (makeVisTracer, visualize, writeGraph)
+import Peras.Abstract.Protocol.Visualizer (makeVisTracer)
 import System.Exit (die)
 
 import qualified Data.Yaml as Y
 import qualified Options.Applicative as O
 
--- FIXME: Make this a subcommand.
 singleMain :: IO ()
 singleMain = mkSimpleScenario >>= simulateNetwork perasTracer >>= print
 
-multinodeMain :: FilePath -> Maybe FilePath -> Maybe FilePath -> Maybe FilePath -> IO ()
-multinodeMain inputFile outputFile logFile visFile =
+multinodeMain :: FilePath -> Maybe FilePath -> Maybe FilePath -> IO ()
+multinodeMain inputFile outputFile logFile =
   do
     input <- either (die . show) pure =<< Y.decodeFileEither inputFile
     (tracer, reader) <- makeVisTracer
@@ -30,22 +29,18 @@ multinodeMain inputFile outputFile logFile visFile =
         Right output -> do
           whenJust outputFile $
             flip Y.encodeFile output
-          events <- reader
           whenJust logFile $
-            flip LBS8.writeFile $
-              LBS8.unlines $
-                A.encode <$> events
-          whenJust visFile $
-            flip writeGraph $
-              visualize events
+            \file -> do
+              events <- reader
+              LBS8.writeFile file . LBS8.unlines $ A.encode <$> events
 
 main :: IO ()
 main =
   do
     Command{..} <- O.execParser commandParser
     case inFile of
-      Nothing -> singleMain
-      Just inFile' -> multinodeMain inFile' outFile traceFile dotFile
+      Nothing -> singleMain -- FIXME: Make this a subcommand.
+      Just inFile' -> multinodeMain inFile' outFile traceFile
 
 whenJust ::
   Applicative m =>
@@ -58,7 +53,6 @@ data Command = Command
   { inFile :: Maybe FilePath
   , outFile :: Maybe FilePath
   , traceFile :: Maybe FilePath
-  , dotFile :: Maybe FilePath
   }
   deriving stock (Eq, Ord, Read, Show)
 
@@ -72,11 +66,9 @@ commandParser =
             (O.long "out-file" <> O.metavar "FILE" <> O.help "Path to output YAML file containing final simulation configuration.")
           <*> (O.optional . O.strOption)
             (O.long "trace-file" <> O.metavar "FILE" <> O.help "Path to output JSON-array file containing simulation trace.")
-          <*> (O.optional . O.strOption)
-            (O.long "dot-file" <> O.metavar "FILE" <> O.help "Path to output GraphViz DOT file containing visualiization.")
    in O.info
         ( O.helper
-            <*> O.infoOption ("peras-iosim " <> showVersion version) (O.long "version" <> O.help "Show version.")
+            <*> O.infoOption ("peras-simulate " <> showVersion version) (O.long "version" <> O.help "Show version.")
             <*> commandOptions
         )
         ( O.fullDesc
