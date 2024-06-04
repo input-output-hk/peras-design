7aebd51eeeb71209b0a7088002e24ffa3da24581
Arnaud Bailly <79840582+abailly-iohk@users.noreply.github.com>
2024-06-04 10:18:17 +0200

    Add job to publish Agda specification as HTML (#122)
    * Add job to publish Agda specification as HTML
    * Remove unneeded bindgen from nix shell

--- flake.nix
+++ flake.nix
@@ -29,33 +29,4 @@
       systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
       # debug = false;
     };
-
-  # This flake is currently failing on aarch64-darwin with:
-  #
-  # error: builder for '/nix/store/f7gai4c7rgsylckm9660rwq8405lf57a-glibc-nolibgcc-2.37-8.drv' failed with exit code 1;
-  #        last 10 log lines:
-  #        > checking for egrep... /nix/store/cdmbbylvs8hr92b41a6v1ds874z2rmz2-gnugrep-3.11/bin/grep -E
-  #        > mips nios2 or1k powerpc riscv s390 sh checking for grep that handles long lines and -e... (cached) /nix/store/cdmbbylvs8hr92b41a6v1ds874z2rmz2-gnugrep-3.11/bin/grep
-  #        > checking for egrep... (cached) /nix/store/cdmbbylvs8hr92b41a6v1ds874z2rmz2-gnugrep-3.11/bin/grep -E
-  #        > sparc x86_64
-  #        > configure: error:
-  #        > *** The GNU C library is currently unavailable for this platform.
-  #        > *** If you are interested in seeing glibc on this platform visit
-  #        > *** the "How to submit a new port" in the wiki:
-  #        > ***   https://sourceware.org/glibc/wiki/#Development
-  #        > *** and join the community!
-  #        For full logs, run 'nix log /nix/store/f7gai4c7rgsylckm9660rwq8405lf57a-glibc-nolibgcc-2.37-8.drv'.
-  # error (ignored): error: cannot unlink '/private/tmp/nix-build-Agda-lib-Agda-2.6.4.1.drv-0': Directory not empty
-  # error: 1 dependencies of derivation '/nix/store/yf47kdqz8pslv22bfa66hlc0wv9jslfj-peras-design-env.drv' failed to build
-
-  # Users should be instructed to put those values in `/etc/nix/conf`, the
-  # following lines works only if your current user to trusted-users, but this is
-  # essentially equivalent to giving that user root access to the system.
-  #
-  # nixConfig = {
-  #   extra-substituters = [ "https://cache.iog.io" ];
-  #   extra-trusted-public-keys =
-  #     [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
-  #   allow-import-from-derivation = true;
-  # };
 }
