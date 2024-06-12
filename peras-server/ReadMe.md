6ce0925346d94a3fa7810d6ee6b9c02a4b859928
Brian W Bush <brian.bush@iohk.io>
2024-06-12 08:40:34 -0600

    Standalone app for running simulation from CLI.

--- peras-server/ReadMe.md
+++ peras-server/ReadMe.md
@@ -1,7 +1,10 @@
-# Peras simulation server
+# Peras server apps
 
 
-## Usage
+## Peras simulation server
+
+
+### Usage
 
 ```console
 $ peras-server --help
@@ -22,17 +25,93 @@ Available options:
 ```
 
 
-## Security
+### Security
 
 - If not username/password pairs are supplied on the command line, then no authorization is required.
 - The WebSocket connection does not require authorization.
 
 
-## Example
+### Example
 
 ```bash
 cabal run exe:peras-server -- \
   --port 8092 \
   --username peras --password prototype \
   --username iohk  --password cardano
+```
+
+
+## Peras CLI app
+
+Run a simulation from the command line.
+
+```console
+$ peras app --help
+
+peras-app: simulate Peras protocol
+
+Usage: peras-app [--version] --in-file FILE [--out-file FILE] 
+                 [--trace-file FILE]
+
+  This command-line tool simulates the Peras protocol.
+
+Available options:
+  -h,--help                Show this help text
+  --version                Show version.
+  --in-file FILE           Path to input YAML file containing initial simulation
+                           configuration.
+  --out-file FILE          Path to output YAML file containing final simulation
+                           configuration.
+  --trace-file FILE        Path to output JSON-array file containing simulation
+                           trace.
+```
+
+
+### Example
+
+```console
+$ cat example-input.yaml
+
+duration: 900
+parties: 7
+u: 20
+a: 200
+r: 10
+k: 17
+l: 10
+tau: 3
+b: 10
+t: 15
+committee: 5
+delta: 0
+activeSlots: 0.1
+rngSeed: 42
+
+$ peras-app --in-file example-input.yaml --out-file example-output.json --trace-file example-trace.jsonarray
+
+$ head example-trace.jsonarray 
+
+{"parameters":{"A":200,"B":10,"K":17,"L":10,"R":10,"T":15,"U":20,"Δ":0,"τ":3},"tag":"Protocol"}
+{"roundNumber":0,"slot":1,"tag":"Tick"}
+{"newChains":[],"newVotes":[],"partyId":1,"tag":"NewChainAndVotes"}
+{"newChains":[],"newVotes":[],"partyId":2,"tag":"NewChainAndVotes"}
+{"newChains":[],"newVotes":[],"partyId":3,"tag":"NewChainAndVotes"}
+{"newChains":[],"newVotes":[],"partyId":4,"tag":"NewChainAndVotes"}
+{"newChains":[],"newVotes":[],"partyId":5,"tag":"NewChainAndVotes"}
+{"newChains":[],"newVotes":[],"partyId":6,"tag":"NewChainAndVotes"}
+{"newChains":[],"newVotes":[],"partyId":7,"tag":"NewChainAndVotes"}
+{"roundNumber":0,"slot":2,"tag":"Tick"}
+
+$ jq . example-output.json | head 
+
+{
+  "diffuser": {
+    "delay": 0,
+    "pendingChains": {},
+    "pendingVotes": {
+      "915": [
+        {
+          "blockHash": "f9094b4c51114a53",
+          "creatorId": 1,
+          "proofM": "a0bae03967ffc586",
 ```
---
+++ peras-server/app/App.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Main where
+
+import Control.Concurrent.Class.MonadSTM.TVar (newTVarIO)
+import Data.Aeson (FromJSON, ToJSON)
+import qualified Data.Aeson as A (encode, encodeFile)
+import qualified Data.ByteString.Lazy.Char8 as LBS8 (unlines, writeFile)
+import Data.Default (def)
+import Data.Version (showVersion)
+import qualified Data.Yaml as Y (decodeFileEither)
+import GHC.Generics (Generic)
+import qualified Options.Applicative as O
+import Paths_peras_server (version)
+import Peras.Abstract.Protocol.Network (SimControl (delay, pause, stop), simulate)
+import Peras.Abstract.Protocol.Network.Arbitrary (genSimConfigIO)
+import Peras.Abstract.Protocol.Types (PerasParams (..))
+import Peras.Abstract.Protocol.Visualizer (makeVisTracer)
+import System.Exit (die)
+
+data AppControl = Simulate
+  { duration :: Integer
+  , parties :: Integer
+  , u :: Integer
+  , a :: Integer
+  , r :: Integer
+  , k :: Integer
+  , l :: Integer
+  , tau :: Integer
+  , b :: Integer
+  , t :: Integer
+  , committee :: Integer
+  , delta :: Integer
+  , activeSlots :: Double
+  , rngSeed :: Int
+  }
+  deriving (Eq, Generic, Show)
+
+instance FromJSON AppControl
+instance ToJSON AppControl
+
+main :: IO ()
+main =
+  do
+    Command{..} <- O.execParser commandParser
+    Simulate{..} <- either (die . show) pure =<< Y.decodeFileEither inFile
+    (tracer, reader) <- makeVisTracer
+    controlVar <- newTVarIO $ def{delay = 0, stop = False, pause = False}
+    simConfig <-
+      genSimConfigIO
+        def{perasU = u, perasA = a, perasR = r, perasK = k, perasL = l, perasτ = tau, perasB = b, perasT = t, perasΔ = delta}
+        activeSlots
+        parties
+        committee
+        (fromIntegral duration)
+        rngSeed
+    simulate tracer controlVar simConfig
+      >>= \case
+        Left e -> die $ show e
+        Right output -> do
+          whenJust outFile $
+            flip A.encodeFile output
+          whenJust traceFile $
+            \file -> do
+              events <- reader
+              LBS8.writeFile file . LBS8.unlines $ A.encode <$> events
+
+whenJust ::
+  Applicative m =>
+  Maybe a ->
+  (a -> m ()) ->
+  m ()
+whenJust = flip . maybe $ pure ()
+
+data Command = Command
+  { inFile :: FilePath
+  , outFile :: Maybe FilePath
+  , traceFile :: Maybe FilePath
+  }
+  deriving (Eq, Ord, Read, Show)
+
+commandParser :: O.ParserInfo Command
+commandParser =
+  let commandOptions =
+        Command
+          <$> O.strOption
+            (O.long "in-file" <> O.metavar "FILE" <> O.help "Path to input YAML file containing initial simulation configuration.")
+          <*> (O.optional . O.strOption)
+            (O.long "out-file" <> O.metavar "FILE" <> O.help "Path to output YAML file containing final simulation configuration.")
+          <*> (O.optional . O.strOption)
+            (O.long "trace-file" <> O.metavar "FILE" <> O.help "Path to output JSON-array file containing simulation trace.")
+   in O.info
+        ( O.helper
+            <*> O.infoOption ("peras-app " <> showVersion version) (O.long "version" <> O.help "Show version.")
+            <*> commandOptions
+        )
+        ( O.fullDesc
+            <> O.progDesc "This command-line tool simulates the Peras protocol."
+            <> O.header "peras-app: simulate Peras protocol"
+        )
