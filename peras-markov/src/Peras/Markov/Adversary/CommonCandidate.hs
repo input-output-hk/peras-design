3781ecb267cfbd723a1fe2c1c4340fed167e95db
Brian W Bush <brian.bush@iohk.io>
2024-06-13 15:31:05 -0600

    Tests for polynomials.

---
+++ peras-markov/src/Peras/Markov/Adversary/CommonCandidate.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Peras.Markov.Adversary.CommonCandidate (
+  Divergence (..),
+  summarize,
+  separatedChains,
+  scenario,
+) where
+
+import NumericPrelude.Base
+import NumericPrelude.Numeric
+
+import Data.Default (Default (def))
+import Data.Foldable (Foldable)
+import Data.Functor ((<$>))
+import Data.Map (Map)
+import Peras.Markov.Adversary (transitions)
+import Prettyprinter (Doc, Pretty (pretty), fill, vsep, (<+>), (<>))
+
+import qualified Algebra.Additive as Additive (C)
+import qualified Algebra.Ring as Ring (C)
+import qualified Data.Map.Strict as Map
+
+newtype Divergence a = MkDivergence
+  { lengths :: Map (Int, Int) a
+  }
+  deriving stock (Eq, Ord, Read, Show)
+
+instance Functor Divergence where
+  fmap f = MkDivergence . Map.map f . lengths
+
+instance Foldable Divergence where
+  foldr = flip flip lengths . ((.) .) . Map.foldr'
+
+instance Ring.C a => Default (Divergence a) where
+  def = MkDivergence $ Map.singleton (0, 0) one
+
+instance (Eq a, Ring.C a, Pretty a) => Pretty (Divergence a) where
+  pretty MkDivergence{lengths} =
+    let pretty' ((lHonest, lAdversary), polynomial) = fill 15 (pretty lHonest) <+> fill 19 (pretty lAdversary) <+> pretty polynomial
+        header =
+          [ fill 15 (pretty "Honest length") <+> fill 19 (pretty "Adversary length") <+> pretty "Probability"
+          , fill 15 (pretty "-------------") <+> fill 19 (pretty "----------------") <+> pretty "-----------"
+          ]
+        rows = pretty' <$> Map.toList lengths
+     in vsep $ header <> rows
+
+summarize :: (Additive.C a, Pretty a) => Divergence a -> Doc ann
+summarize MkDivergence{lengths} =
+  let
+    summary = Map.mapKeysWith (+) (uncurry compare) lengths
+    pretty' (cmp, polynomial) = fill 30 (pretty $ show cmp <> " & adversary candidate") <+> pretty polynomial
+    header =
+      [ fill 30 (pretty "Honest vs adversary length") <+> pretty "Probability"
+      , fill 30 (pretty "--------------------------") <+> pretty "-----------"
+      ]
+    rows = pretty' <$> Map.toList summary
+   in
+    vsep $ header <> rows
+
+transitionImpl :: Additive.C a => (a -> a -> (Int, Int) -> a -> Map (Int, Int) a) -> a -> a -> Divergence a -> Divergence a
+transitionImpl transition' p q = MkDivergence . Map.foldrWithKey' ((Map.unionWith (+) .) . transition' p q) Map.empty . lengths
+
+separatedChains :: Ring.C a => a -> a -> Divergence a -> Divergence a
+separatedChains =
+  transitionImpl $ \p q (lHonest, lAdversary) weight ->
+    Map.fromList
+      [ ((lHonest + 1, lAdversary), p * (one - q) * weight)
+      , ((lHonest, lAdversary + 1), (one - p) * q * weight)
+      , ((lHonest + 1, lAdversary + 1), p * q * weight)
+      , ((lHonest, lAdversary), (one - p) * (one - q) * weight)
+      ]
+
+-- | Find the distribution of chain lengths after U + L slots, given
+-- that an adversarial block has been produced in the first U slots.
+scenario ::
+  Ring.C a =>
+  -- | Peras U
+  Int ->
+  -- | Peras L
+  Int ->
+  -- | Probability of honest block
+  a ->
+  -- | Probability of adversarial block
+  a ->
+  -- | Joint distribution of chain lengths and the probability that the adversary didn't produce a block in the first U slots.
+  (Divergence a, a)
+scenario perasU perasL p q =
+  let
+    -- Run the chains forward U slots.
+    interim = transitions p q perasU separatedChains def
+    -- Discard chains that have not produced an adversary block yet.
+    interimWithAdversarialBlock = MkDivergence . Map.filterWithKey (const . (/= 0) . snd) $ lengths interim
+    noBlock = Map.foldrWithKey (\k v acc -> if snd k == 0 then acc + v else acc) zero $ lengths interim
+    -- Run the chains forward K slots.
+    result = transitions p q perasL separatedChains interimWithAdversarialBlock
+   in
+    (result, noBlock)
---
+++ peras-markov/test/Peras/Markov/PolynomialSpec.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Peras.Markov.PolynomialSpec where
+
+import NumericPrelude.Base
+import NumericPrelude.Numeric
+
+import Control.Applicative (pure, (<*>))
+import Control.Arrow (Arrow (second, (***)))
+import Data.Functor ((<$>))
+import Number.Ratio ((%))
+import Peras.Markov.Orphans ()
+import Peras.Markov.Polynomial (
+  Monomial (MkMonomial),
+  Polynomial (MkPolynomial),
+  Term (MkTerm),
+  eval,
+  evalMonomial,
+  evalTerm,
+ )
+import Prettyprinter (Pretty (pretty), (<>))
+import Test.Hspec (Expectation, Spec, describe, it, shouldBe, shouldSatisfy)
+import Test.QuickCheck (
+  Arbitrary (arbitrary),
+  Gen,
+  Testable (property),
+  choose,
+  counterexample,
+  forAll,
+  listOf,
+ )
+
+import qualified Algebra.Absolute as Absolute (C)
+import qualified Algebra.Ring as Ring (C)
+import qualified Data.Map.Strict as Map
+
+spec :: Spec
+spec = do
+  describe "Polynomial arithmetic identical to rational arithmetic" $
+    do
+      it "Monomials" $
+        property $
+          forAll ((,) <$> genRationalPQ <*> genMonomial) $ \((p, q), (concrete, symbolic)) -> do
+            counterexample ("Polynomial: " <> show (pretty symbolic)) $
+              evalMonomial p q symbolic `shouldBe` concrete p q
+      it "Terms" $
+        property $
+          forAll ((,) <$> genRationalPQ <*> (second (uncurry MkTerm) <$> genTerm)) $ \((p, q), (concrete, symbolic)) -> do
+            counterexample ("Polynomial: " <> show (pretty symbolic)) $
+              evalTerm p q symbolic `shouldBe` concrete p q
+      it "Polynomials" $
+        property $
+          forAll ((,) <$> genRationalPQ <*> genPolynomial) $ \((p, q), (concrete, symbolic)) -> do
+            counterexample ("Polynomial: " <> show (pretty symbolic)) $
+              eval p q symbolic `shouldBe` concrete p q
+  describe "Polynomial arithmetic identical to floating-point arithmetic" $
+    do
+      it "Monomials" $
+        property $
+          forAll ((,) <$> genDoublePQ <*> genMonomial) $ \((p, q), (concrete, symbolic)) -> do
+            counterexample ("Polynomial: " <> show (pretty symbolic)) $
+              evalMonomial p q symbolic `shouldBeApproximately` concrete p q
+      it "Terms" $
+        property $
+          forAll ((,) <$> genDoublePQ <*> (second (uncurry MkTerm) <$> genTerm)) $ \((p, q), (concrete, symbolic)) -> do
+            counterexample ("Polynomial: " <> show (pretty symbolic)) $
+              evalTerm p q symbolic `shouldBeApproximately` concrete p q
+      it "Polynomials" $
+        property $
+          forAll ((,) <$> genDoublePQ <*> genPolynomial) $ \((p, q), (concrete, symbolic)) -> do
+            counterexample ("Polynomial: " <> show (pretty symbolic)) $
+              eval p q symbolic `shouldBeApproximately` concrete p q
+
+shouldBeApproximately :: Double -> Double -> Expectation
+shouldBeApproximately x y = abs (x - y) `shouldSatisfy` (< 1e-9)
+
+genRationalPQ :: Gen (Rational, Rational)
+genRationalPQ =
+  do
+    p <- do
+      d <- choose (1, 1000000)
+      n <- choose (0, d)
+      pure $ n % d
+    pure (p, 1 - p)
+
+genDoublePQ :: Gen (Double, Double)
+genDoublePQ =
+  do
+    p <- choose (0, 1)
+    pure (p, 1 - p)
+
+instance Show (a -> a -> a) where
+  show _ = "<<a function>>"
+
+genMonomial :: Ring.C a => Gen (a -> a -> a, Monomial)
+genMonomial =
+  do
+    i <- choose (0, 5)
+    j <- choose (0, 5)
+    pure (\p q -> p ^ toInteger i * q ^ toInteger j, MkMonomial i j)
+
+genTerm :: (Arbitrary a, Ring.C a) => Gen (a -> a -> a, (Monomial, a))
+genTerm =
+  do
+    c <- arbitrary
+    ((\f p q -> c * f p q) *** (,c)) <$> genMonomial
+
+genPolynomial :: (Arbitrary a, Ring.C a) => Gen (a -> a -> a, Polynomial a)
+genPolynomial =
+  ((\fs p q -> sum $ (\f -> f p q) <$> fs) *** MkPolynomial . Map.fromListWith (+)) . unzip <$> listOf genTerm
