6312321d0055d22907b866f74a4fe7f802d75865
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-06-21 13:16:07 +0200

    Simplify benchmarks

--- peras-simulation/bench/Bench.hs
+++ peras-simulation/bench/Bench.hs
@@ -15,63 +15,42 @@ main :: IO ()
 main = do
   voters <- generate $ genVoters 10000
   defaultMain
-    [ bgroup
-        "Committee Selection"
-        [benchCommitteeSelection n voters | n <- [100, 500, 1000, 5000, 10000]]
-    , benchVote voters
+    [ benchVote voters
     , benchVerification voters
     ]
 
 benchVerification :: [Voter] -> Benchmark
 benchVerification voters =
   bench "Single Verification" $
-    perRunEnv setup $ \ ~(blockHash, totalStake, input, committeeSize, spos, vote) ->
+    perRunEnv setupVote $ \ ~(blockHash, totalStake, input, committeeSize, spos, vote) ->
       evaluate $
         rnf $
           checkVote committeeSize totalStake spos input vote
  where
-  setup = generate $ do
-    blockHash <- gen32Bytes
-    input <- fromBytes <$> gen32Bytes
-    let totalStake = sum $ voterStake <$> voters
-        committeeSize = fromInteger $ totalStake * 75 `div` 100
-        roundNumber = 42
-        spos = mkStakeDistribution voters
-    voter <- elements voters
-    let vote = fromJust $ castVote blockHash totalStake input committeeSize roundNumber voter
-    pure (blockHash, totalStake, input, committeeSize, spos, vote)
+  setupVote = do
+    setup voters >>= \(blockHash, totalStake, input, committeeSize, roundNumber, voters) -> do
+      voter <- generate $ elements voters
+      let vote = fromJust $ castVote blockHash totalStake input committeeSize roundNumber voter
+          spos = mkStakeDistribution voters
+      pure (blockHash, totalStake, input, committeeSize, spos, vote)
 
 benchVote :: [Voter] -> Benchmark
 benchVote voters =
   bench "Single Voting" $
-    perRunEnv setup $ \ ~(blockHash, totalStake, input, committeeSize, roundNumber, voter) ->
+    perRunEnv setupVoter $ \ ~(blockHash, totalStake, input, committeeSize, roundNumber, voter) ->
       evaluate $
         rnf $
           castVote blockHash totalStake input committeeSize roundNumber voter
  where
-  setup = generate $ do
-    blockHash <- gen32Bytes
-    input <- fromBytes <$> gen32Bytes
-    let totalStake = sum $ voterStake <$> voters
-        committeeSize = fromInteger $ totalStake * 75 `div` 100
-        roundNumber = 42
-    voter <- elements voters
-    pure (blockHash, totalStake, input, committeeSize, roundNumber, voter)
+  setupVoter = do
+    setup voters >>= \(blockHash, totalStake, input, committeeSize, roundNumber, voters) -> do
+      voter <- generate $ elements voters
+      pure (blockHash, totalStake, input, committeeSize, roundNumber, voter)
 
-benchCommitteeSelection :: Int -> [Voter] -> Benchmark
-benchCommitteeSelection numVoters voters =
-  let
-    label = show numVoters <> " voters"
-    setup = generate $ do
-      blockHash <- gen32Bytes
-      input <- fromBytes <$> gen32Bytes
-      let totalStake = sum $ voterStake <$> voters
-          committeeSize = fromInteger $ totalStake * 75 `div` 100
-          roundNumber = 42
-      pure (blockHash, totalStake, input, committeeSize, roundNumber)
-   in
-    env setup $ \ ~(blockHash, totalStake, input, committeeSize, roundNumber) ->
-      bench label $
-        nf
-          (mapMaybe (castVote blockHash totalStake input committeeSize roundNumber))
-          (take numVoters voters)
+setup voters = generate $ do
+  blockHash <- gen32Bytes
+  input <- fromBytes <$> gen32Bytes
+  let totalStake = sum $ voterStake <$> voters
+      committeeSize = fromInteger $ totalStake * 75 `div` 100
+      roundNumber = 42
+  pure (blockHash, totalStake, input, committeeSize, roundNumber, voters)
