ad37288f04050bf8d5b31bd9c9bb935b2d57b11e
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-07-19 16:39:09 +0200

    Add VM descriptor to host staging site
    This is perhaps a bit overkill...

--- infra/cloud-run.tf
+++ infra/cloud-run.tf
@@ -39,6 +39,12 @@ resource "google_cloud_run_v2_service" "peras_server" {
       "managed-by" = "github-actions"
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      template, client, client_version
+    ]
+  }
 }
 
 resource "google_cloud_run_v2_service_iam_member" "noauth" {
@@ -59,49 +65,10 @@ resource "google_cloud_run_domain_mapping" "peras_server_domain_mapping" {
   spec {
     route_name = google_cloud_run_v2_service.peras_server.name
   }
-}
-
-# Peras staging web site
-resource "google_cloud_run_v2_service" "peras_staging" {
-  name     = "peras-staging"
-  location = "us-east1"
-  client   = "terraform"
-
-  depends_on = [
-    google_artifact_registry_repository.peras-docker
-  ]
-
-  template {
-    containers {
-      image = "us-east1-docker.pkg.dev/iog-hydra/peras-docker/input-output-hk/peras-site:1ec430738accb34d622f2325ac8a0760331343f7"
-      ports {
-        container_port = 8091
-      }
-    }
-
-    labels = {
-      "commit-sha" = "1ec430738accb34d622f2325ac8a0760331343f7"
-      "managed-by" = "github-actions"
-    }
-  }
-}
-
-resource "google_cloud_run_v2_service_iam_member" "noauth" {
-  location = google_cloud_run_v2_service.peras_staging.location
-  name     = google_cloud_run_v2_service.peras_staging.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
-data "google_project" "project" {}
-
-resource "google_cloud_run_domain_mapping" "peras_staging_domain_mapping" {
-  name     = "peras-staging.cardano-scaling.org"
-  location = google_cloud_run_v2_service.peras_staging.location
-  metadata {
-    namespace = data.google_project.project.project_id
-  }
-  spec {
-    route_name = google_cloud_run_v2_service.peras_staging.name
+  lifecycle {
+    ignore_changes = [
+      metadata,
+      spec
+    ]
   }
 }
