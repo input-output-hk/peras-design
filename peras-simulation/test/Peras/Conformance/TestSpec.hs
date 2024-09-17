20c5a8d018a4d602fe3923c2a9a434ac7f08774b
Brian W Bush <brian.bush@iohk.io>
2024-09-17 08:02:44 -0600

    Chain-length control via `--qc-max-size`.

--- peras-simulation/test/Peras/Conformance/TestSpec.hs
+++ peras-simulation/test/Peras/Conformance/TestSpec.hs
@@ -7,7 +7,7 @@ import Data.Functor (void)
 import Peras.Conformance.Test.Prototype (prop_node)
 import Test.Hspec (Spec, describe)
 import Test.Hspec.QuickCheck (modifyMaxSuccess, prop)
-import Test.QuickCheck (Blind (Blind), Gen, Property, Testable, expectFailure, property)
+import Test.QuickCheck (Blind (Blind), Gen, Property, Testable, arbitrary, expectFailure, forAll, property, resize)
 import Test.QuickCheck.DynamicLogic (DL, anyAction, anyActions_, forAllDL)
 import Test.QuickCheck.Gen.Unsafe (Capture (..), capture)
 import Test.QuickCheck.Monadic (PropertyM, assert, monadic')
@@ -18,4 +18,4 @@ spec :: Spec
 spec =
   describe "Prototype node"
     . prop "Simulation respects model"
-    $ forAllDL (replicateM_ 20 anyAction >> anyActions_) (prop_node . Blind)
+    $ forAll arbitrary prop_node
